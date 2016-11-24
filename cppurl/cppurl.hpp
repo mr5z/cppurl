2ffbf1bfe4d8ab9324f49c8dde60c47c4521c25f
Mark Anthony <mr5.4short@gmail.com>
2016-11-24 08:54:26 +0800

    add file uploads feature

--- cppurl/cppurl.hpp
+++ cppurl/cppurl.hpp
@@ -1,8 +1,11 @@
+#include <sys/stat.h>
 #include <curl/curl.h>
 #include <string>
 #include <thread>
 #include <vector>
 #include <functional>
+#include <stdexcept>
+#include <map>
 
 class Http {
 	using ResponseCallback = std::function<void(CURLcode, const std::string&)>;
@@ -27,25 +30,47 @@ public:
 	}
 
 	/**
-	 * @param callback(CURLcode, response)
+	 * Perform a request with a GET method
+	 * @param const std::string &url URL to be requested
+	 * @param callback(CURLcode code, const std::string &response) response callback invoked from other thread
 	 */
-	void request(const std::string &url, ResponseCallback callback) {
-		response = "";
-		curl_easy_reset(handle);
-		curl_easy_setopt(handle, CURLOPT_WRITEDATA, &response);
-		curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, &Http::writeCallback);
-		curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+	void get(const std::string &url, ResponseCallback callback) {
+		initRequest(url);
 		std::thread([this, callback] {
 			CURLcode res = curl_easy_perform(handle);
 			callback(res, response);
 		}).detach();
 	}
 
-	void post(const std::string &url, ResponseCallback callback) {
-		std::thread([this, callback] {
+	/**
+	* Perform a request with a POST method
+	* @param const std::string &url: URL to be requested
+	* @param std::map<std::string, std::string> files: list of pair of file name and file path
+	* @param callback(CURLcode code, const std::string &response): response callback invoked from other thread
+	*/
+	void post(const std::string &url, std::map<std::string, std::string> files, ResponseCallback callback) {
+		curl_httppost *formpost = nullptr;
+		curl_httppost *lastptr = nullptr;
+		curl_slist *headerlist = nullptr;
+
+		for (auto &i : files) {
+			curl_formadd(&formpost,
+				&lastptr,
+				CURLFORM_COPYNAME, i.first.c_str(),
+				CURLFORM_FILE, i.second.c_str(),
+				CURLFORM_END);
+		}
+
+		headerlist = curl_slist_append(headerlist, "Content-Type: multipart/form-data");
+		initRequest(url);
+		curl_easy_setopt(handle, CURLOPT_HTTPPOST, formpost);
+		std::thread([this, formpost, headerlist, callback] {
 			CURLcode res = curl_easy_perform(handle);
 			callback(res, response);
+			curl_formfree(formpost);
+			curl_slist_free_all(headerlist);
 		}).detach();
+
 	}
 
 private:
@@ -53,6 +78,15 @@ private:
 		data->append((char*)contents, size * nmemb);
 		return size * nmemb;
 	}
+
+	void initRequest(const std::string &url) {
+		response = "";
+		curl_easy_reset(handle);
+		curl_easy_setopt(handle, CURLOPT_WRITEDATA, &response);
+		curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, &Http::writeCallback);
+		curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+	}
+
 private:
 	CURL *handle;
 	std::string response;
